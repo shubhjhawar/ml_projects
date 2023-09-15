f8c553e8556831d97cee71268cd6ec0b968b15b5
Shubh <shubhjhawar78@gmail.com>
2023-09-15 16:28:08 -0400

    first commit

---
+++ pages/_app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "../app/globals.css";
+
+const Home = () => {
+  return (
+    <div className='font-bold text-3xl underline'>Home</div>
+  )
+}
+
+export default Home
